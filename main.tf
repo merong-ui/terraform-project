56ebf652965f11540db63683b360c412eca0b761
Meron <girmameron32@gmail.com>
2026-01-01 19:23:15 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region  = "us-east-1"
+}
+
+resource "aws_vpc" "development-vpc" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = "10.0.10.0/24"
+    availability_zone = "us-east-1a"
+}
+
+data "aws_vpc" "existing_vpc" {
+  default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+    vpc_id = data.aws_vpc.existing_vpc.id
+    cidr_block = "172.31.96.0/20"
+    
+}
