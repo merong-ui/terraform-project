f94782867bba31e5be7c2e7c7e35302016223f0f
Meron <girmameron32@gmail.com>
2026-01-27 06:09:26 -0500

    add terraform remote backend

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+    requires_version = ">= 0.12"
+    backend "s3" {
+      bucket = "myapp-bucket"
+      key = "myapp/state.tfstate"
+      region = "us-east-1"
+    }
+    
+}
 provider "aws" {
     region  = "us-east-1"
 }
